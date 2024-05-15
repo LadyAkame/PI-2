b6a0734bf7f72d239276edb01362f6ce4cb98999
GustavoRodriguesk <161630558+GustavoRodriguesk@users.noreply.github.com>
2024-05-15 15:54:01 -0300

    esse tá ok ?

---
+++ 150524/pi2car-log-cad/script/script.js
@@ -0,0 +1,8 @@
+
+    document.getElementById("meuBotao").onclick = function() {
+        window.location.href = "ingressos.html";
+    };
+    
+    document.getElementById("meuBotao1").onclick = function() {
+        window.location.href = "logincad/log-cad.html";
+    };

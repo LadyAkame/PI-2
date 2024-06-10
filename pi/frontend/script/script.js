c44fe5cef04c7339b17c6a3c1dfee286e65e6b3b
GustavoRodriguesk <rodrigues.gustavo2@aluno.ifsp.edu.br>
2024-06-10 15:52:49 -0300

    calma

--- pi/frontend/script/script.js
+++
@@ -1,8 +0,0 @@
-
-    document.getElementById("meuBotao").onclick = function() {
-        window.location.href = "cadastro.html";
-    };
-    
-    document.getElementById("meuBotao1").onclick = function() {
-        window.location.href = "login.html";
-    };

32ecaec7aabe816e5cfa7e86a0c14f75aba21f1c
GustavoRodriguesk <rodrigues.gustavo2@ifsp.edu.br>
2024-06-10 12:13:25 -0300

    quase lá

--- 150524/pi2car-log-cad/script/script.js
+++
@@ -1,8 +0,0 @@
-
-    document.getElementById("meuBotao").onclick = function() {
-        window.location.href = "ingressos.html";
-    };
-    
-    document.getElementById("meuBotao1").onclick = function() {
-        window.location.href = "logincad/log-cad.html";
-    };
---
+++ pi/frontend/script/script.js
@@ -0,0 +1,8 @@
+
+    document.getElementById("meuBotao").onclick = function() {
+        window.location.href = "cadastro.html";
+    };
+    
+    document.getElementById("meuBotao1").onclick = function() {
+        window.location.href = "login.html";
+    };
